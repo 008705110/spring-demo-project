9936e320e66803cc74f83df9aaae9c770cd08c20
schalla <schalla@adobe.com>
2022-02-22 10:28:37 -0700

    Adding Sonar

--- src/main/java/com/example/cognixia/CognixiaApplication.java
+++ src/main/java/com/example/cognixia/CognixiaApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CognixiaApplication {
-
+  int test = 1;
+  public String notUsed = "cognixia";
+  
 	public static void main(String[] args) {
 		SpringApplication.run(CognixiaApplication.class, args);
 	}
